ac94041acfd716b0f525f3d722e210c6fd997bc5
Elishma Talkar <elishmatalkar@gmail.com>
2026-03-27 01:50:27 +0530

    style: update MotionButton to black with white hover expansion

--- src/components/ui/MotionButton.tsx
+++ src/components/ui/MotionButton.tsx
@@ -18,18 +18,18 @@ const MotionButton: FC<Props> = ({ label, classes }) => {
     return (
         <button
             className={cn(
-                'group relative h-auto w-64 cursor-pointer rounded-full border-none bg-transparent p-0 outline-none',
+                'group relative h-auto w-64 cursor-pointer rounded-full border-none bg-black p-0 outline-none overflow-hidden',
                 classes
             )}
         >
             <span
-                className='absolute left-0 top-1/2 -translate-y-1/2 m-0 block h-12 w-12 bg-white rounded-full duration-500 ease-in-out group-hover:w-full'
+                className='absolute left-0 top-1/2 -translate-y-1/2 m-0 block h-12 w-12 bg-white rounded-full duration-500 ease-in-out group-hover:w-full group-hover:h-full'
                 aria-hidden='true'
             ></span>
-            <div className='absolute top-1/2 left-3 translate-x-0 -translate-y-1/2 duration-500 ease-in-out group-hover:translate-x-[0.4rem] flex items-center justify-center w-6 h-6'>
+            <div className='absolute top-1/2 left-3 translate-x-0 -translate-y-1/2 duration-500 ease-in-out group-hover:translate-x-[0.4rem] flex items-center justify-center w-6 h-6 z-10'>
                 <ArrowRight className='text-black size-5' />
             </div>
-            <span className='relative block py-3 pl-12 pr-6 text-white font-bold text-lg tracking-tight whitespace-nowrap duration-500 group-hover:text-black text-left'>
+            <span className='relative block py-3 pl-14 pr-6 text-white font-bold text-lg tracking-tight whitespace-nowrap duration-500 group-hover:text-black text-left z-10'>
                 {label}
             </span>
         </button>
