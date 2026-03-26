84a8bc65b6cc3903d0266c216eb3c628280efda2
Elishma Talkar <elishmatalkar@gmail.com>
2026-03-27 01:46:14 +0530

    style: change MotionButton to black and white theme

--- src/components/ui/MotionButton.tsx
+++ src/components/ui/MotionButton.tsx
@@ -23,13 +23,13 @@ const MotionButton: FC<Props> = ({ label, classes }) => {
             )}
         >
             <span
-                className='absolute left-0 top-1/2 -translate-y-1/2 m-0 block h-12 w-12 bg-[#5b6af5] rounded-full duration-500 ease-in-out group-hover:w-full'
+                className='absolute left-0 top-1/2 -translate-y-1/2 m-0 block h-12 w-12 bg-white rounded-full duration-500 ease-in-out group-hover:w-full'
                 aria-hidden='true'
             ></span>
             <div className='absolute top-1/2 left-3 translate-x-0 -translate-y-1/2 duration-500 ease-in-out group-hover:translate-x-[0.4rem] flex items-center justify-center w-6 h-6'>
-                <ArrowRight className='text-white size-5' />
+                <ArrowRight className='text-black size-5' />
             </div>
-            <span className='relative block py-3 pl-12 pr-6 text-[#f2f4ff] font-bold text-lg tracking-tight whitespace-nowrap duration-500 group-hover:text-white text-left'>
+            <span className='relative block py-3 pl-12 pr-6 text-white font-bold text-lg tracking-tight whitespace-nowrap duration-500 group-hover:text-black text-left'>
                 {label}
             </span>
         </button>
